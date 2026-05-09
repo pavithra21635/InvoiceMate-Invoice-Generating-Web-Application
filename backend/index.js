17b78c4b184afc2b47b2c82920012f0b45421b05
pavithra21635 <pavithra21635@gmail.com>
2026-05-09 11:51:58 +0530

    T01-Creating invoice data mode l & implementing mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-// backend/index.js
-const express = require('express');
-const app = express();
-const PORT = 5000;
+// // backend/index.js
+// const express = require('express');
+// const app = express();
+// const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('InvoiceMate Backend is running!');
-});
+// app.get('/', (req, res) => {
+//   res.send('InvoiceMate Backend is running!');
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
