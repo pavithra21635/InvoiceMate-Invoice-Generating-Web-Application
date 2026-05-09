1a8dee838976f82e15d074162f03a9015caeaa95
pavithra21635 <pavithra21635@gmail.com>
2026-05-09 15:36:04 +0530

    T04-Add timer to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <div style={{ height: "100vh", width: "100vw" }}>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,28 +1,34 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom";
 import loginImage from "../assets/loginpageimage.png";
 import logo from "../assets/Logo.png";
 
 
 export default function LoginPage() {
-  return (
-    // login-page equivalent: flex-row, centered, full screen
-    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen w-full bg-[#E1C6FC] overflow-hidden">
-      
-      {/* login-section */}
-      <div className="bg-white p-8 rounded-[5px] shadow-lg w-full max-w-[532px] min-h-[692px] z-10">
-        <img className="block mx-auto h-32 w-[550px] h-[550px]" src={logo} alt="Logo" />
-        
-        
-      
-       
-      </div>
+    const navigate = useNavigate();
 
-      {/* login-image-section (Hidden on small mobile, visible on tablet/desktop) */}
-      <img 
-        className="hidden md:block rounded-[5px] shadow-lg w-full max-w-[632px] h-[692px] object-cover -ml-4" 
-        src={loginImage} 
-        alt="Login Illustration" 
-      />
-    </div>
-  );
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        navigate("/dashboard");
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
+    return (
+        
+        <div className="flex flex-col md:flex-row items-center justify-center min-h-screen w-full bg-[#E1C6FC] overflow-hidden">
+        
+        <div className="bg-white p-8 rounded-[5px] shadow-lg w-full max-w-[532px] min-h-[692px] z-10">
+            <img className="block mx-auto h-32 w-[550px] h-[550px]" src={logo} alt="Logo" />
+        </div>
+
+        <img 
+            className="hidden md:block rounded-[5px] shadow-lg w-full max-w-[632px] h-[692px] object-cover -ml-4" 
+            src={loginImage} 
+            alt="Login Illustration" 
+        />
+        </div>
+    );
 }
